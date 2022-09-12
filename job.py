e963def22b957c168a98d2f5284f5792acbdf27b
Fe-Nik-S <mikifeynman@gmail.com>
2022-09-12 10:03:18 +0300

    #6 Updated the description

--- job.py
+++ job.py
@@ -1,5 +1,5 @@
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0):
+    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
         pass
 
     def run(self):
